7cffec80f5baf7aa12e1ce267dba2104b021d823
Constantin Kühne <constantin.kuehne@student.hdbw-hochschule.de>
2021-11-30 10:37:13 +0100

    changed .gitignore

--- load-data/load_data.sync.py
+++ load-data/load_data.sync.py
@@ -1,18 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:percent
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.13.1
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
 # %%
 import os
 import elasticsearch as es
