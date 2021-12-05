54f663c0eb1735df1723e98d483119d52a7babe2
Constantin Kühne <constantin.kuehne@student.hdbw-hochschule.de>
2021-12-05 18:12:38 +0100

    added file for later implementation of ASIN query

---
+++ backend/src/elastic/query-asin.ts
@@ -0,0 +1,3 @@
+export default (ASIN: string): string => {
+    return ASIN
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,14 @@
 import express from "express";
+import queryASIN from "./elastic/query-asin"
 const app = express();
 const port = 3000; // default port to listen
 
 // define a route handler for the default home page
-app.get( "/", ( _, res ) => {
-    res.send( "Hello world!" );
+app.get( "/", ( _ , res ) => {
+    res.send(queryASIN("test"));
 } );
 
 // start the Express server
 app.listen( port, () => {
     console.log( `server started at http://localhost:${ port }` );
-} );
+});
