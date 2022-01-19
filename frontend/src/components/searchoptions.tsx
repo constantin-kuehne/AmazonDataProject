ecd958d4d49f629a35cc24445ac3ff35b65aacbd
Tim Muscholl <tim.muscholl@gmail.com>
2022-01-19 15:36:43 +0100

    Update Search Options 1.1

--- frontend/src/components/searchoptions.tsx
+++ frontend/src/components/searchoptions.tsx
@@ -4,30 +4,64 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
+import {alpha, createStyles, makeStyles, Theme} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        options: {
+            position: "relative",
+            borderRadius: theme.shape.borderRadius,
+            backgroundColor: alpha(theme.palette.common.white, 0.15),
+            "&:hover": {
+                backgroundColor: alpha(theme.palette.common.white, 0.25),
+            },
+            padding: 0,
+            margin: 0,
+            marginLeft: 0,
+            width: "100%",
+            height: "100%",
+            [theme.breakpoints.up("sm")]: {
+                marginLeft: theme.spacing(1),
+                width: "auto",
+            },
+        }
+
+        }
+    )
+
+    );
+
 
 export default function SearchSelect() {
     const [search, setSearch] = React.useState('');
+
+    const classes = useStyles();
 
     const handleChange = (event: SelectChangeEvent) => {
         setSearch(event.target.value as string);
     };
 
     return (
-        <Box sx={{ minWidth: 120 }}>
-            <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label" color="secondary"> Option</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={search}
-                    label="Search Option"
-                    onChange={handleChange}
-                    color="secondary"
-                >
-                    <MenuItem value={10}>ASIN</MenuItem>
-                    <MenuItem value={20}>Product</MenuItem>
-                </Select>
-            </FormControl>
-        </Box>
+       <div className={classes.options}>
+           <Box  sx={{ minWidth: 120 }}>
+               <FormControl fullWidth>
+                   <InputLabel id="demo-simple-select-label" color="secondary"> Option</InputLabel>
+                   <Select
+                       labelId="demo-simple-select-label"
+                       id="demo-simple-select"
+                       value={search}
+                       label="Search Option"
+                       onChange={handleChange}
+                       color="secondary"
+                   >
+                       <MenuItem value={10}>ASIN</MenuItem>
+                       <MenuItem value={20}>Product</MenuItem>
+                   </Select>
+               </FormControl>
+           </Box>
+
+
+
+       </div>
     );
 }
--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -6,16 +6,7 @@ import Animations from "../animations/loading_animation";
 export default function About(){
     return(
         <div>
-            <div>
-                <h4>Bar chart </h4>
-                <br/>
-                <ul>
-                    <li>Top 20 Reviews des gesuchten Produktes mit den meisten Bewertungen.  </li>
-                    <li>In diesem Bar Chart werden die Top 20 der meistbewerteten Reviews gezeigt. Die Balken sind nach der Anzahl an total Votes sortiert - der Anteil der helpful Votes wird durch eine andere Sättigung der Farbe hervorgehoben</li>
-                    <li>Dieser Chart soll dem User eine Übersicht über die besten Reviews geben, wie hilfreich sie waren oder auch nicht. Umso mehr positive Bewertungen ein Review hat, desto mehr kann man davon ausgehen, dass es sich nicht um ein gefälschtes Review handelt.  </li>
-
-                </ul>
-            </div>
+            <p> about coming soon</p>
         </div>
 
 
