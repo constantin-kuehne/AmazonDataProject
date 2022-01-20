e3e960cfb04f5900f67393f7964ca4f5a2c61713
Tim Muscholl <tim.muscholl@gmail.com>
2022-01-20 15:23:07 +0100

    Update Graph Descriptions 1.1

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -13,26 +13,31 @@ export const Dashboard = ({
   return (
     <div>
       <h3> Top 20 most voted reviews </h3>
-        <p> </p>
+        <p> In diesem Bar Chart werden die Top 20 der meistbewerteten Reviews gezeigt. Die Balken sind nach der Anzahl an total Votes sortiert - der Anteil der helpful Votes wird durch eine andere Sättigung der Farbe hervorgehoben. </p>
+        <p> Dieser Chart gibt einen Überblick über die besten Reviews und wie hilfreich sie waren. Je höher die Anzahl der Bewertungen ist, desto geringer ist die Wahrscheinlichkeit, dass es sich um ein gefälschtes Review handelt</p>
       <BarChart searchedProduct={searchedProduct} width={1000} height={500} />
 
-      <h3> Number of reviews per month.</h3>
-        <p> </p>
+      <h3> Number of reviews per month</h3>
+        <p>Dieser Line Graph zeigt die pro Monat geschriebenen Reviews über den Verlauf der Zeit. Die Zeitachse beginnt in dem Monat, in dem das erste Review geschrieben wurde, meist der Monat, in dem das Produkt auf den Markt kam.  </p>
+        <p>Durch starkes Ansteigen oder Absteigen der Anzahl an Reviews lassen sich gewisse Trends erkennen. Beispielsweise, ob ein Produkt gerade beliebt ist oder ob es ein Ersatzprodukt gibt. </p>
       <LineChart
         searchedProduct={searchedProduct}
         datetype={"MONTH"}
         width={1000}
         height={500}
       />
-      <h3> Amount of helpful votes.</h3>
-        <p> </p>
+      <h3> Amount of helpful votes</h3>
+        <p>Der zweite Scatter Plot in unserem Dashboard ist darauf ausgelegt, die Qualität der Reviews darzustellen. Auf der x-Achse wird der prozentuale Anteil der helpful Votes an den total Votes dargestellt, auf der y-Achse wird wieder die Anzahl an Reviews gezeigt. </p>
+        <p>Das gesuchte Produkt wird auch hier durch einen orangen Punkt gekennzeichnet. Die anderen Punkte, die der Scatter plot darstellt, sind wieder die 100 ähnlichsten Produkte derselben Kategorie. </p>
       <ScatterPlot
         searchedProduct={searchedProduct}
         width={1000}
         height={500}
       />
       <h3> Star-rating & amount of reviews  </h3>
-        <p> </p>
+        <p>Dieser Scatter Plot zeigt auf der x-Achse die durchschnittliche Anzahl an Sternebewertungen (0-5), auf der y-Achse wird angezeigt, wie viele Reviews, dass Produkt insgesamt hat.  </p>
+        <p>Das gesuchte Produkt wird durch einen orangen Punkt gekennzeichnet. Die anderen Punkte, die der Scatter plot darstellt, sind die 100 ähnlichsten Produkte derselben Kategorie.  </p>
+        <p>Die Position des gesuchten Produktes (orange) soll die Beliebtheit des Produktes widerspiegeln, umso weiter oben rechts in der Ecke sich das Produkt befindet, desto beliebter ist es bei anderen Kunden. </p>
       <ScatterPlotTwo
         searchedProduct={searchedProduct}
         width={1000}
