d61930c96776a2739693b3274dc9c22d844d4470
Constantin Kühne <constantin.kuehne@student.hdbw-hochschule.de>
2022-01-20 14:32:36 +0100

    added searched Product name to about section

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -2,17 +2,16 @@ import { Box } from "@material-ui/core";
 import { Skeleton } from "@mui/material";
 import React from "react";
 import Animations from "../animations/loading_animation";
+import { Source } from "../components/searchbar";
 
-export default function About(){
-    return(
-        <div>
-            <p> about coming soon</p>
-        </div>
-
-
-
-    );
-
-
-
+export default function About({
+  searchedProduct,
+}: {
+  searchedProduct: Source | null;
+}) {
+  return (
+    <div>
+      <p>{searchedProduct?.product_title}</p>
+    </div>
+  );
 }
