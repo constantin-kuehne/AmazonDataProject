2cd9b539b218dc42de616ab583f55de089d9c340
Tim Muscholl <tim.muscholl@gmail.com>
2022-01-20 13:40:22 +0100

    Update Graphs Descriptions

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -12,22 +12,27 @@ export const Dashboard = ({
 }) => {
   return (
     <div>
-      <h3> This is the first Chart.</h3>
+      <h3> Top 20 most voted reviews </h3>
+        <p> </p>
       <BarChart searchedProduct={searchedProduct} width={1000} height={500} />
-      <h3> This is the second Chart.</h3>
+
+      <h3> Number of reviews per month.</h3>
+        <p> </p>
       <LineChart
         searchedProduct={searchedProduct}
         datetype={"MONTH"}
         width={1000}
         height={500}
       />
-      <h3> This is the third Chart.</h3>
+      <h3> Amount of helpful votes.</h3>
+        <p> </p>
       <ScatterPlot
         searchedProduct={searchedProduct}
         width={1000}
         height={500}
       />
-      <h3> This is the fourth Chart.</h3>
+      <h3> ? </h3>
+        <p> </p>
       <ScatterPlotTwo
         searchedProduct={searchedProduct}
         width={1000}
